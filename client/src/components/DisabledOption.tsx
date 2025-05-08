2dc96f3cfd1fe2fc45b1b5e795a2d1cf814ea666
Manmohan Wable <manmohanwable0@gmail.com>
2025-05-08 20:14:37 +0530

    Maximum votes option will highlight green whereas minimum votes option will highlight red. When votes are equal, both options will highlight yellow

--- client/src/components/DisabledOption.tsx
+++ client/src/components/DisabledOption.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 interface OptionProps {
     text : string,
     optionVote : number, 
@@ -5,9 +7,24 @@ interface OptionProps {
 }
 export default function Option ({ text, optionVote, total } :  OptionProps) {
 
-    return <div data-value={text} className={`text-center m-2 w-fit rounded-md bg-gray-500 text-white min-w-40 cursor-not-allowed`}>
-        <div className="bg-blue-500 h-full p-2 rounded-md" style={{ width: `${optionVote/total * 100}%` }}>
-            {text}
+    const [color, setColor] = useState<"bg-green-400" | "bg-red-400" | "bg-yellow-400">("bg-yellow-400")
+
+    useEffect(() => {
+        if (optionVote / total > 0.5) {
+            setColor("bg-green-400")
+        } else if (optionVote / total < 0.5) {
+            setColor("bg-red-400")
+        } else {
+            setColor("bg-yellow-400")
+        }
+    }, [optionVote, total])
+
+    return <div data-value={text} className={`relative flex m-2 h-12 rounded-md bg-gray-500 text-white min-w-40 cursor-not-allowed`}>
+        <div className={`${color} absolute flex items-center justify-between h-full p-2 rounded-md`} style={{ width: `${optionVote/total * 100}%` }}>
+        </div>
+        <div className="relative z-10 flex w-full items-center justify-between h-full px-4">
+            <span className="whitespace-nowrap">{text}</span>
+            <span className="whitespace-nowrap">{`${(optionVote / total * 100).toFixed(2)} %`}</span>
         </div>
     </div>
 }
